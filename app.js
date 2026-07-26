8dacea43e7cdd8accaafb207b809cb9b561ce56b
aungaungaug15 <aungaungaug15@gmail.com>
2026-07-26 23:32:58 +0630

    Upload website code for Railway

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const htmlTemplate = (phoneNumber, messagesHtml) => `
 <!DOCTYPE html>
